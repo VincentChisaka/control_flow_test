b78de387b097ac5ed7e88da33f46ce30e24c726a
VincentChisaka <vincentchisaka2@gmail.com>
2023-02-11 07:46:41 +0300

    integers count

---
+++ ruby.rb
@@ -0,0 +1,13 @@
+# Mtokyo's code.
+
+
+print "Integer only: "
+user_num = Integer(get.chomp)
+
+if user_num < 0
+    puts "This is a negative integer."
+elsif user_num > 0
+    puts "This is a positive integer."
+else
+    puts "You picked zero."
+end
